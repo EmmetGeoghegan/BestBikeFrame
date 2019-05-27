56a51c3e85ae0247bbd07d8e4c67c73f4e81d7b1
Emmet Geoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-05-27 15:01:43 +0100

    Added Node points

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
+import random
+from scipy.spatial import Delaunay
 
 
 def draw_graph(xpts, ypts, trusslist):
-    plt.scatter(xpts, ypts, s=100, color="blue")
-    for i in range(0, len(xpts), 1):
-        plt.annotate(i, (xpts[i], ypts[i]), size=20, color="black")
-    plt.title("Our Structure")
-    plt.xlabel("x-Coords")
-    plt.ylabel("y-Coords")
+    # Draw Trusses
     for i in trusslist:
         start = i[0]
         end = i[1]
-        plt.plot([xpts[start], xpts[end]], [ypts[start], ypts[end]], color="blue", linewidth=2)
+        plt.plot([xpts[start], xpts[end]], [ypts[start], ypts[end]], color="blue", linewidth=2, zorder=1)
+    # Draw Hinges
+    plt.scatter(xpts, ypts, s=100, color="blue")
+    plt.scatter(xpts[0], ypts[0], s=100, color="red", zorder=2)
+    plt.scatter(xpts[-1], ypts[-1], s=100, color="red", zorder=2)
+    # Draw Hinge labels
+    for i in range(0, len(xpts), 1):
+        plt.annotate(i, (xpts[i], ypts[i]), size=20, color="black", zorder=3)
+    plt.title("Our Structure")
+    plt.xlabel("x-Coords")
+    plt.ylabel("y-Coords")
     plt.show()
 
 
@@ -28,10 +35,19 @@ xpts = []
 ypts = []
 trusslist = []
 
-add_hinge(5, 5)
-add_hinge(6, 6)
-add_hinge(-10, 6)
+for i in range(0, 5):
+    add_hinge(random.randint(-10, 10), random.randint(-10, 10))
+
+
+"""add_hinge(-10, -5)
+add_hinge(-4, 5)
+add_hinge(4, -5)
+add_hinge(10, 7)
 
 add_truss(0, 1)
 add_truss(0, 2)
+add_truss(1, 2)
+add_truss(3, 2)
+add_truss(1, 3)
+"""
 draw_graph(xpts, ypts, trusslist)
