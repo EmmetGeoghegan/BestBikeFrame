442219bbcff331d8a7f8ba134edec4ce40791b5e
Emmet Geoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-05-27 15:31:08 +0100

    Added Delaunay Triangulation
    Triangulation now working

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import random
 from scipy.spatial import Delaunay
 
 
-def draw_graph(xpts, ypts, trusslist):
+def draw_graph(hingelist, trusslist):
+    # Unzip and get our x and y co-ords
+    xpts, ypts = zip(*hingelist)
     # Draw Trusses
-    for i in trusslist:
-        start = i[0]
-        end = i[1]
-        plt.plot([xpts[start], xpts[end]], [ypts[start], ypts[end]], color="blue", linewidth=2, zorder=1)
+    triangles = Delaunay(hingelist)
+    for i in range(0, len(hingelist), 1):
+        plt.triplot(xpts, ypts, triangles.simplices.copy(), zorder=1, color="green")
     # Draw Hinges
-    plt.scatter(xpts, ypts, s=100, color="blue")
-    plt.scatter(xpts[0], ypts[0], s=100, color="red", zorder=2)
-    plt.scatter(xpts[-1], ypts[-1], s=100, color="red", zorder=2)
+    plt.scatter(xpts, ypts, s=100, color="blue", zorder=2)
     # Draw Hinge labels
-    for i in range(0, len(xpts), 1):
-        plt.annotate(i, (xpts[i], ypts[i]), size=20, color="black", zorder=3)
+    for i in range(0, len(hingelist), 1):
+        plt.annotate(i, (hingelist[i][0], hingelist[i][1]), size=20, color="black", zorder=3)
     plt.title("Our Structure")
     plt.xlabel("x-Coords")
     plt.ylabel("y-Coords")
@@ -23,31 +22,23 @@ def draw_graph(xpts, ypts, trusslist):
 
 
 def add_hinge(x, y):
-    xpts.append(x)
-    ypts.append(y)
+    hingelist.append([x, y])
 
 
 def add_truss(pt1, pt2):
     trusslist.append([pt1, pt2])
 
 
-xpts = []
-ypts = []
+def main():
+    for i in range(0, 5):
+        add_hinge(random.randint(-10, 10), random.randint(-10, 10))
+    draw_graph(hingelist, trusslist)
+
+
+hingelist = []
 trusslist = []
 
-for i in range(0, 5):
-    add_hinge(random.randint(-10, 10), random.randint(-10, 10))
-
-
-"""add_hinge(-10, -5)
-add_hinge(-4, 5)
-add_hinge(4, -5)
-add_hinge(10, 7)
-
-add_truss(0, 1)
-add_truss(0, 2)
-add_truss(1, 2)
-add_truss(3, 2)
-add_truss(1, 3)
-"""
-draw_graph(xpts, ypts, trusslist)
+while True:
+    main()
+    hingelist = []
+    trusslist = []
