b5aecb2c86d79f9541074c5c7f4088dbc778d13e
EmmetGeoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-05-29 20:14:42 +0100

    Finishing Touches to Generalization Problem
    - Added finishing touches
    - GPU based render method in plotwithgpu.py
    - Commented up  code

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from mpl_toolkits.mplot3d import axes3d
-import time
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 from itertools import combinations
 import math as math
 from anastruct import SystemElements
-import numpy as np
+# import plotwithgpu as gpuplot # Un-Comment if you have an Nvida GPU
+# 2 Classes, 7 Functions and
+
+###########
+# Classes #
+###########
 
 
-class Node:
+class Node:        # Class to create our nodes and give them properties
     num_of_nodes = 0
 
     def __init__(self, xpos, ypos, anchor):
@@ -17,14 +21,16 @@ class Node:
         self.anchor = anchor
         Node.num_of_nodes += 1
 
+    # Gets the coords of  a node
     def coords(self):
         return([self.xpos, self.ypos])
 
+    # Applies a force to a given node
     def applyforce(self, force):
         self.force = force
 
 
-class Truss:
+class Truss:     # Class that creates Trusses and gives them properites
     num_of_truss = 0
     available_mats = 0
 
@@ -40,24 +46,34 @@ class Truss:
         Truss.num_of_truss += 1
         Truss.available_mats -= Truss.length(self)
 
+    # Gets the slope of a Truss
     def tslope(self):
         slope = self.rise/self.run
         return slope
 
+    # Gets the angle to the vertical of the Truss
     def tangv(self):
         ang = math.degrees(math.atan(Truss.tslope(self)))
         return ang
 
+    # Gets the length of the truss
     def length(self):
         dist = math.sqrt(self.rise**2 + self.run**2)
         return dist
 
+    # Gets the midpoint of the truss
     def midpoint(self):
         x = (self.startx + self.endx)/2
         y = (self.starty + self.endy)/2
         return (x, y)
 
-############################################################################################################################
+####################
+# Truss Generation #
+####################
+
+# This function creates our Truss start and end points by
+# Forming Delaunay triangles. The start and end points for each edge
+# Are passed to the Truss class to generate our objects
 
 
 def triangleify(coords):
@@ -78,15 +94,18 @@ def triangleify(coords):
         output.append(Truss(i[0], i[1]))
     return output
 
+########################
+# Structure Simulation #
+########################
 
-###########################################################################################################################################
 
-
+# Function gets the anastruct node ID's of a given node in the model
 def getnodeid(node):
     id = ss.find_node_id(node.coords())
     return(id)
 
 
+# Function to add Trusses and the fixed and rolling supports to the model
 def addobj(trusslist, Nodelist):
     for i in trusslist:
         ss.add_truss_element(location=[[i.startx, i.starty], [i.endx, i.endy]], EA=8*(10**11))
@@ -98,6 +117,7 @@ def addobj(trusslist, Nodelist):
             ss.add_support_roll(node_id=getnodeid(Nodelist[i]), direction=1)
 
 
+# Function to add a force to the force node (anchor type 3)
 def nodeforce(Nodelist, force):
     for i in range(0, len(Nodelist), 1):
         if Nodelist[i].anchor == 3:
@@ -105,6 +125,7 @@ def nodeforce(Nodelist, force):
             ss.point_load(node_id=getnodeid(Nodelist[i]), Fx=0, Fy=Nodelist[i].force, rotation=0)
 
 
+# Our Fitness Function, Determines the score of the force nodes location
 def stiffness(force, nodelist):
     for i in range(0, len(nodelist), 1):
         if nodelist[i].anchor == 3:
@@ -112,36 +133,35 @@ def stiffness(force, nodelist):
             delx = newpos["ux"]
             dely = newpos["uy"]
             dist = math.sqrt(delx**2 + dely**2)
-            # return(force/dist)
             return(dist)
 
-##############################################################################################################
 
-
+# Function that defines our nodes. Takes paramaters to place the force node (x,y)
 def definenodelist(x, y, nodelist):
-    ss = SystemElements()
     nodelist = []
     # Back Wheel (Stationary Mount)
     nodelist.append(Node(0, 0, 1))
     # Front Wheel (Moving Mount)
     nodelist.append(Node(70, 45, 2))
     # Pedal Mount (Normal Node)
-    nodelist.append(Node(30, 0, 0))
+    nodelist.append(Node(30, 0, 3))
     # Force Node
-    nodelist.append(Node(x, y, 3))
+    nodelist.append(Node(x, y, 0))
     # Available Material
-    Truss.available_mats = 300
+    Truss.available_mats = 90.2
     return nodelist
 
 
+# Function that preps the list of all node co-ordinates to be turned into truss start and end points
 def definecoordlist(nodelist):
     for i in Nodelist:
         coordlist.append(Node.coords(i))
     return(coordlist)
 
 
-ss = SystemElements()
-
+###################
+# Node Generation #
+###################
 sval = []
 poscoordsx = []
 poscoordsy = []
@@ -173,6 +193,9 @@ for i in range(20, 40, 1):
 
 print(currentsmallest, bestcoords)
 
+################################
+# Graphical Physics Simulation #
+################################
 ss = SystemElements()
 Nodelist = []
 Nodelist = definenodelist(bestcoords[0], bestcoords[1], Nodelist)
@@ -185,57 +208,22 @@ trusslist = triangleify(coordlist)
 force = 10000000000
 addobj(trusslist, Nodelist)
 nodeforce(Nodelist, force)
-ss.solve(max_iter=5)
-stiff = stiffness(force, Nodelist)
-if stiff < currentsmallest:
-    currentsmallest = stiff
-    bestcoords = (i, j)
-sval.append(stiffness(force, Nodelist))
-poscoordsx.append(i)
-poscoordsy.append(j)
-
+ss.solve(max_iter=50)
 ss.show_structure()
 
-
+###########################
+# Plot Values Graphically #
+###########################
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-# load some test data for demonstration and plot a wireframe
-# Make data.
 X = poscoordsx
 Y = poscoordsy
 Z = sval
-ax.plot_trisurf(X, Y, Z)
+ax.plot_trisurf(X, Y, Z, cmap="jet")
+plt.show()
+print(Truss.available_mats)
 
-# rotate the axes and update
-for angle in range(0, 360):
-    ax.view_init(30, angle)
-    plt.show()
-    plt.pause(.001)
-
-"""from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Make data.
-X = poscoordsx
-Y = poscoordsy
-Z = sval
-print(X)
-print(Y)
-print(Z)
-# Plot the surface.
-ax.plot_trisurf(X, Y, Z)
-
-for angle in range(0, 360):
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.001)
-"""
+########################
+# Plot Values With GPU #
+########################
+# gpuplot.plsplotgpu(Z)
---
+++ plotwithgpu.py
@@ -0,0 +1,32 @@
+import sys
+from vispy import app, scene
+import numpy as np
+
+
+def plsplotgpu(Z):
+    Z = [Z]
+    Z = np.reshape(Z, (20, 99))
+    canvas = scene.SceneCanvas(keys='interactive', bgcolor='w')
+    view = canvas.central_widget.add_view()
+    view.camera = scene.TurntableCamera(up='z', fov=60)
+
+    p1 = scene.visuals.SurfacePlot(z=Z, color=(0.3, 0.3, 1, 1))
+
+    view.add(p1)
+
+    xax = scene.Axis(pos=[[-0.5, -0.5], [0.5, -0.5]], tick_direction=(0, -1),
+                     font_size=16, axis_color='k', tick_color='k', text_color='k',
+                     parent=view.scene)
+    xax.transform = scene.STTransform(translate=(0, 0, -0.2))
+
+    yax = scene.Axis(pos=[[-0.5, -0.5], [-0.5, 0.5]], tick_direction=(-1, 0),
+                     font_size=16, axis_color='k', tick_color='k', text_color='k',
+                     parent=view.scene)
+    yax.transform = scene.STTransform(translate=(0, 0, -0.2))
+
+    canvas.show()
+    app.run()
+
+
+def testfunc():
+    print("Hello")
